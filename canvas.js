b7e00bdb247fd0b4aa58224d36bd3473fca6cc99
mdmoshiur <moshiur1176@gmail.com>
2019-12-05 21:22:04 +0600

    basic canvas design finished :)

--- canvas.js
+++ canvas.js
@@ -1,22 +1,27 @@
 var canvas = document.getElementById('canvas');
 var c = canvas.getContext('2d');
 var w = window.innerWidth;
-var h = 0.5*window.innerHeight;
+var h = window.innerHeight;
 canvas.width = w;
 canvas.height = h;
+//colors
+var colors = ['#FF0000', '#01DF01', '#0101DF', '#FF0040', '#000000','#2EFEF7','#B404AE','#fff','#FFFF00'];
+//Turn transparency on
+c.globalAlpha = .2;
 
-function Circle(x, y, radius, dx, dy){
+function Circle(x, y, radius, dx, dy, fillColor){
   this.x = x;
   this.y = y;
   this.radius = radius;
   this.dx = dx;
   this.dy = dy;
+  this.fillColor = fillColor;
 
   this.draw = function() {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
-    c.strokeStyle = '#00FF00'
-    c.stroke();
+    c.fillStyle = this.fillColor;
+    c.fill();
   }
 
   this.update = function() {
@@ -33,14 +38,14 @@ function Circle(x, y, radius, dx, dy){
 
 var circleArray = [];
 
-for( var i = 0; i<50; i++){
-  var radius = Math.random()* 20 + 10;
+for( var i = 0; i<100; i++){
+  var radius = Math.random()* 20 + 5;
   var x = Math.random() * (w - radius * 2) + radius;
   var y = Math.random() * (h - radius * 2) + radius;
-  var dx = (Math.random() - 0.5) * 8;
-  var dy = (Math.random() - 0.5) * 8;
-
-  circleArray.push(new Circle(x,y,radius,dx,dy));
+  var dx = (Math.random() - 0.5) * 4;
+  var dy = (Math.random() - 0.5) * 4;
+  var fillColor = colors[Math.floor(i%colors.length)];
+  circleArray.push(new Circle(x,y,radius,dx,dy,fillColor));
 }
 
 function animate(){
